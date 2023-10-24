07da83ec0af3d10b409b6f14214da6d186a6167f
Jose Del Palacio <148625569+JoseDPalacio@users.noreply.github.com>
2023-10-24 12:01:12 +0200

    Add files via upload
    solo ejercicio 2

--- Ejercicios/C1D2Ejercicio2/src/Main.java
+++ Ejercicios/C1D2Ejercicio2/src/Main.java
@@ -1,3 +1,6 @@
+//Compañero Sol Lereah y Óscar Cuenca Martin
+//EL programa se rompe  porque  no puede convertir el tipo de dato. Esto se genera en todas las situaciones debido a que Java es un lenguaje fuertemente tipado y no puede convertir el tipo de dato una vez asignado
+
 public class Main {
     public static void main(String[] args) {
         //Varibles
@@ -6,12 +9,15 @@ public class Main {
         boolean condicional = true;
         String palabra = "hola";
 
+        System.out.println("Esto es una variable tipo entero: "+entero);
+        System.out.println("Esto es una variable de tipo double: "+numero);
+        System.out.println("Esto es una variable de tipo boolean: "+condicional);
+        System.out.println("Esto es una variable de tipo string: "+palabra);
 
         //Variables pero mal
-       /* int ent = "hola";
+        int ent = "hola";
         String palabra1 = 25;
         String palabra2 = 25.5;
 
-        */
     }
 }
