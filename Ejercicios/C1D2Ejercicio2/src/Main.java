7fb3d9ed045031737217ebbb94f5014f12d9156e
Jose Del Palacio <148625569+JoseDPalacio@users.noreply.github.com>
2023-10-24 11:45:53 +0200

    Add files via upload
    ejercicios día 2

---
+++ Ejercicios/C1D2Ejercicio2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //Varibles
+        int entero = 5;
+        double numero = 5.4;
+        boolean condicional = true;
+        String palabra = "hola";
+
+
+        //Variables pero mal
+       /* int ent = "hola";
+        String palabra1 = 25;
+        String palabra2 = 25.5;
+
+        */
+    }
+}
---
+++ Ejercicios/C1D2Ejercicio4/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        int num1, num2;
+        int suma, resta, multi;
+        double divi;
+        Scanner sc = new Scanner(System.in);
+
+        //Lectura
+        System.out.println("Por favor escriba el primer número");
+        num1 = sc.nextInt();
+
+        System.out.println("Por favor escriba el segundo número");
+        num2 = sc.nextInt();
+
+        //Resultado
+        suma = num1+num2;
+        System.out.println("La suma es: "+suma);
+
+        resta = num1-num2;
+        System.out.println("La resta es: "+resta);
+
+        multi = num1*num2;
+        System.out.println("La multiplicacion es: "+multi);
+
+        divi = (double) num1/num2;
+        System.out.println("La division es: "+divi);
+    }
+}
